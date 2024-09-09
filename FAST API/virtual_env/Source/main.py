6e22ee5e3f1b58d5e4fafe7091dd7fe87acf9a44
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-09-09 19:27:08 +0200

    my first api call

--- FAST API/virtual_env/Source/main.py
+++ FAST API/virtual_env/Source/main.py
@@ -10,6 +10,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/admin")
+def get_admin():
+    return {"Name": "Sicelo"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
